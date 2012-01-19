38855d107c1a8d5715a21781b80ab949a33d716d
Mark Gottscho <mgottscho@ucla.edu>
2012-01-18 16:43:32 -0800

    Working .config with loadable modules & initramfs. Added build and installation script as well.

---
+++ build_and_install_kernel.sh
@@ -0,0 +1,15 @@
+echo '>>>>> Cleaning last build...'
+make clean
+echo '>>>>> Compiling new  kernel...'
+make -j4 > /dev/null
+echo '>>>>> Installing kernel...'
+sudo make install
+echo '>>>>> Installing modules...'
+sudo make modules_install
+echo '>>>>> Updating initramfs...'
+sudo update-initramfs -c -k 3.2.1-MWG+
+echo '>>>>> Cleaning up...'
+sudo rm /boot/*.old
+echo '>>>>> Updating grub...'
+sudo update-grub
+echo '>>>>> Done!'
