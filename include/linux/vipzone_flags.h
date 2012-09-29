a9a0039a034974acd4f3d99304cd80fd5195ac2c
Mark Gottscho <mgottscho@ucla.edu>
2012-09-29 14:14:14 -0700

    Major changes. Found path from page fault handler to alloc_pages_nodemask. Added hooks for vip_flags to ensure the vma-based flags get seen by the physical allocator. Changed all comments labeled MWG to vipzone so we can grep easily for our own code. Modified the vipzone_choose() function to reflect our policy changes, simplified. Added a file for vipzone bit-flags. Compiles, not tested.

---
+++ include/linux/vipzone_flags.h
@@ -0,0 +1,27 @@
+//vipzone
+
+#ifdef CONFIG_VIPZONE_FRONT_END
+#ifndef _VIPZONE_FLAGS
+#define _VIPZONE_FLAGS
+
+#define _VIP_TYP_READ			0x0000 // x0xx xxxx
+#define _VIP_TYP_WRITE			0x0040 // x1xx xxxx
+#define _VIP_TYP_MASK			0x0040 // 0100 0000
+
+#define _VIP_UTIL_LO				0x0000 // 0xxx xxxx
+#define _VIP_UTIL_HI				0x0080 // 1xxx xxxx
+#define _VIP_UTIL_MASK			0x0080 // 1000 0000
+
+#define _VIP_MASK					0x00c0 // 1100 0000 
+
+//Macros for extracting vipzone flags
+//#define is_vip_typ_read(vip_flags) (vip_flags & _VIP_TYP_MASK == _VIP_TYP_READ)
+//#define is_vip_typ_write(vip_flags) (vip_flags & _VIP_TYP_MASK == _VIP_TYP_WRITE)
+
+//#define is_vip_util_lo(vip_flags) (vip_flags & _VIP_UTIL_MASK == _VIP_UTIL_LO)
+//#define is_vip_util_hi(vip_flags) (vip_flags & _VIP_UTIL_MASK == _VIP_UTIL_HI)
+
+//#define is_vip_flags_legal(vip_flags) (vip_flags & ~_VIP_MASK == 0)
+
+#endif
+#endif
